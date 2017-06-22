dd6bc7c568299bf84bb64da3a7d24fb01ba1fcc3
Matthew Cain <msc66@pitt.edu>
2017-06-22 19:00:45 -0400

    Remove manual process.env set

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-process.env.COSMIC_BUCKET='cosmic-real-estate';
 
 app.use(express.static(__dirname + '/public'));
-
 
 require('./app/routes')(app);
 
